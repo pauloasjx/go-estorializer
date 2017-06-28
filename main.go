81417788f98ac6476080e08c654726371cbfd01b
Paulo Alves <pauloalvsjr@gmail.com>
2017-06-28 01:44:29 -0300

    Word by Word

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	//"sync"
 	//"os"
 	//"strconv"
 
@@ -20,6 +21,8 @@ type Visit struct {
 }
 
 func main() {
+	var words []string
+
 	database, err := sql.Open("sqlite3", "./History")
 	if err != nil {
 		panic(err)
@@ -37,53 +40,69 @@ func main() {
 		visits.Scan(&visit.Url, &visit.VisitCount)
 		//fmt.Println(visit.Url + " - " + strconv.Itoa(visit.VisitCount))
 
-		ProcessUrl(visit.Url)
-		return
+		//go ProcessUrl(visit.Url)
+		visit_words := ProcessUrl(visit.Url)
+
+		if visit_words != nil {
+			for _, visit_word := range visit_words {
+				words = append(words, string(visit_word[0]))
+			}
+		}
+
+		fmt.Println(words)
+		//return
+
 	}
 }
 
-func ProcessUrl(url string) {
+func ProcessUrl(url string) [][]string {
 
 	response, err := http.Get(url)
 	if err != nil {
-		panic(err)
-	}
+		fmt.Println(err)
+	} else {
 
-	/*
-		request, err := http.NewRequest("GET", url, nil)
+		/*
+			request, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				panic(err)
+			}
+
+			response, err := http.DefaultClient.Do(request)
+			if err != nil {
+				panic(err)
+			}
+		*/
+
+		defer response.Body.Close()
+
+		body, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			panic(err)
 		}
 
-		response, err := http.DefaultClient.Do(request)
+		body_text, err := html2text.FromString(string(body))
 		if err != nil {
 			panic(err)
 		}
-	*/
 
-	defer response.Body.Close()
+		//var text string
+		//json.Unmarshal([]byte(body_text), &text)
 
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
+		//fmt.Println(text)
+
+		regex, err := regexp.Compile(`(\b[\p{L}A-Za-z]{4,16}\b)`)
+		//regex, err := regexp.Compile(`(\b[\p{L}A-Za-z]{4,}\b)`)
+		//regex, err := regexp.Compile(`(\b[\p{L}A-Za-z]+\b)`)
+		//regex, err := regexp.Compile(`(\b[^\s]+\b)`)
+		if err != nil {
+			panic(err)
+		}
+
+		words := regex.FindAllStringSubmatch(body_text, -1)
+
+		return words
 	}
 
-	body_text, err := html2text.FromString(string(body))
-	if err != nil {
-		panic(err)
-	}
-
-	//var text string
-	//json.Unmarshal([]byte(body_text), &text)
-
-	//fmt.Println(text)
-
-	regex, err := regexp.Compile(`(\b[^\s]+\b)`)
-	if err != nil {
-		panic(err)
-	}
-
-	words := regex.FindAllStringSubmatch(body_text, -1)
-
-	fmt.Println(words)
+	return nil
 }
