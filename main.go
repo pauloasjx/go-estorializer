7b54142b98f2b7daa00ad577f05fd0b25f363145
Paulo Alves <pauloalvsjr@gmail.com>
2017-06-28 00:22:16 -0300

    Printing Plain Text from Urls

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"strconv"
+	"io/ioutil"
+	"net/http"
+	//"os"
+	//"strconv"
 
+	"github.com/jaytaylor/html2text"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -29,10 +33,37 @@ func main() {
 
 	for visits.Next() {
 		visits.Scan(&visit.Url, &visit.VisitCount)
-		fmt.Println(visit.Url + " - " + strconv.Itoa(visit.VisitCount))
+		//fmt.Println(visit.Url + " - " + strconv.Itoa(visit.VisitCount))
+
+		ProcessUrl(visit.Url)
+		return
 	}
 }
 
-func ProcessUrl() {
+func ProcessUrl(url string) {
 
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		response, err := http.DefaultClient.Do(request)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	body_text, err := html2text.FromString(string(body))
+
+	fmt.Println(body_text)
 }
